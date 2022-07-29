71b9ea228cefbb10e8b1996b543e3d598d2ec1fc
codingiove <2023221317@qq.com>
2022-07-29 17:38:43 +0800

    new

--- 练习代码/Vue3/vuex/src/views/11_核心五Modules-命名空间.vue
+++ 练习代码/Vue3/vuex/src/views/11_核心五Modules-命名空间.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
+    <h1>{{ $store.state.name }}</h1>
     <h1>{{ $store.state.home.count }}</h1>
     <h1>{{ $store.getters["home/doubleCount"] }}</h1>
     <button @click="increment">count++</button>
+    <button @click="changeRootName">changeRootName</button>
   </div>
 </template>
 
@@ -22,5 +24,8 @@ console.log(store.state.home.count);
 
 function increment() {
   store.dispatch("home/incrementCountAction");
+}
+function changeRootName() {
+  store.dispatch("home/changeRootName", "the shy");
 }
 </script>
