ff724948dff0b329ca1d124704c5301c07e26f5a
codingiove <2023221317@qq.com>
2022-08-18 20:14:26 +0800

    wxss-wxml-wxs

---
+++ 练习代码/小程序/pages/05_learn_wxs/index.js
@@ -0,0 +1,4 @@
+// pages/05_learn_wxs/index.js
+Page({
+
+});
--- 练习代码/小程序/pages/index/index.js
+++ 练习代码/小程序/pages/index/index.js
@@ -6,6 +6,9 @@ Page({
     pages: [
       { name: "01_注册页面", path: "/pages/01_register_page/index" },
       { name: "02_内置组件", path: "/pages/02_common_component/index" },
+      { name: "03_学习WXSS", path: "/pages/03_learn_wxss/index" },
+      { name: "04_学习WXML", path: "/pages/04_learn_wxml/index" },
+      { name: "05_学习WXS", path: "/pages/05_learn_wxs/index" },
     ],
   },
   onBtnClick(event) {
